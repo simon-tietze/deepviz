0d8bb82fb51b2cfd5cab9685cec542fc8b5d9e44
Simon Tietze <simontietze@ben-energy.com>
2019-03-19 17:22:16 +0100

    added output dim

--- R/model_nodes.R
+++ R/model_nodes.R
@@ -6,7 +6,7 @@
 model_nodes <- function(x){
   assert_that(is.keras_model(x))
   if (is.keras_model_sequential(x)) {
-    model_layers <- x$get_config()$layers
+    model_layers <- x$get_config()
     l_name <- map_chr(model_layers, ~purrr::pluck(., "config", "name"))
   } else {
     model_layers <- x$get_config()$layers
@@ -16,16 +16,25 @@ model_nodes <- function(x){
 
   l_activation <- model_layers %>%
     map_chr(
-      ~(purrr::pluck(., "config", "activation") %||% "")
+      ~(purrr::pluck(., "config", "activation")  %||% "")
     )
+
+  l_output_shape <- x$layers %>%
+    map_chr(
+      ~ paste0("[", paste(reticulate::py_to_r(.x$output_shape)[-1], collapse = " x "), "]") %||% "[?]"
+    )
+
+  l_activation_formatted <-
+    map(l_activation, ~ if_else(.x != "", paste0(" (", .x, ")"), ""))
 
   create_node_df(
     n = length(model_layers),
     name = l_name,
     type = l_type,
-    label = glue::glue("{l_name}\n{l_type}\n{l_activation}"),
+    label = glue::glue("{l_name}\n{l_type}{l_activation_formatted}\n{l_output_shape}"),
     shape = "rectangle",
-    activation = l_activation
+    activation = l_activation,
+    output_shape = l_output_shape
   )
 }
 
